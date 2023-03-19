e54e7decd5eb108577aa75841a252bc04e37ca22
Dawid Wozniak <dawidwozniak0805@gmail.com>
2023-03-19 01:26:13 +0100

    Add footer and modal

---
+++ src/components/Footer.tsx
@@ -0,0 +1,52 @@
+import styled, { css } from "styled-components"
+import { Modal } from "./Modal"
+
+type FooterProps = { name?: string }
+
+const FooterNode = styled.div`
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  cursor: pointer;
+  padding: 0px 5px;
+  color: ${css`var(--text-color)`};
+  font-size: 14px;
+`
+
+const TextFooterModal = styled.div`
+  font-size: 14px;
+  padding: 5px;
+`
+
+export const Footer = (props: FooterProps) => {
+  const link = (
+    <FooterNode>
+      {props.name} &#174; {new Date().getFullYear()}
+    </FooterNode>
+  )
+  const content = (
+    <>
+      <div style={{ textAlign: "center", padding: "10px" }}>
+        <h1>GitTruck Beta 0.02</h1>
+        <TextFooterModal>
+          GitHub repository:{" "}
+          <a href="https://github.com/dawidwoz/git-truck/tree/main">github.com/dawidwoz/git-truck/tree/main</a>
+        </TextFooterModal>
+        <TextFooterModal>
+          We use following assets:
+          <br /> 
+          <a href="https://www.flaticon.com/free-icons/pull" title="pull icons">
+            Pull icons created by edt.im - Flaticon
+          </a>
+        </TextFooterModal>
+      </div>
+    </>
+  )
+  return (
+    <Modal link={link} content={content}></Modal>
+  )
+}
+
+Footer.defaultProps = {
+  name: "Git Truck Beta"
+}
